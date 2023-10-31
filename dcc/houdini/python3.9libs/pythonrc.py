df77813e6f83e17712f45b6efe67b88bea3b51d5
Jonathan Cox <jcox@jon-macbook.local>
2023-10-31 07:57:14 +0000

    Tidy up bootstrap

--- dcc/houdini/python3.9libs/pythonrc.py
+++ dcc/houdini/python3.9libs/pythonrc.py
@@ -7,6 +7,7 @@ import sys
 import hou
 
 from node_manager import manager
+from node_manager import utils
 
 
 logging.basicConfig(
@@ -15,20 +16,28 @@ logging.basicConfig(
     stream=sys.stdout,
 )
 
-# # Disk based load
-# os.environ["NODE_MANAGER_REPOS"] = "/Users/jcox/hdas"
-# os.environ["NODE_MANAGER_BASE"] = "/Users/jcox/hdas"
-# manager.initialise_node_manager(
-#     background=hou.isUIAvailable(),
-#     edit_plugin="EditDirectory",
-# )
 
-# Git based load
-os.environ["NODE_MANAGER_REPOS"] = "git@github.com:j0nc0x/hda_repo.git"
-# os.environ["NODE_MANAGER_BASE"] = "/Users/jcox/hdas_git"
-manager.initialise_node_manager(
-    background=hou.isUIAvailable(),
-    load_plugin="GitLoad",
-    edit_plugin="EditDirectory",
-    release_plugin="GitRelease",
-)
+
+if utils.using_rez():
+    os.environ["NODE_MANAGER_REPOS"] = "http://gitea.ad.dupevfx.com/joco/houdini_hdas.git"
+else:
+    os.environ["NODE_MANAGER_PLUGINS_PATH"] = "/Users/jcox/source/github/node_manager/lib/python/node_manager/plugins"
+
+    # Disk based load
+    os.environ["NODE_MANAGER_REPOS"] = "/Users/jcox/hdas"
+    os.environ["NODE_MANAGER_BASE"] = "/Users/jcox/hdas"
+    manager.initialise_node_manager(
+        background=hou.isUIAvailable(),
+        edit_plugin="EditDirectory",
+    )
+
+    # # Git based load
+    # os.environ["NODE_MANAGER_REPOS"] = "git@github.com:j0nc0x/hda_repo.git"
+    # os.environ["NODE_MANAGER_BASE"] = "/Users/jcox/hdas_git"
+
+    # manager.initialise_node_manager(
+    #     background=hou.isUIAvailable(),
+    #     load_plugin="GitLoad",
+    #     edit_plugin="EditDirectory",
+    #     release_plugin="GitRelease",
+    # )
