93ff4e0b4597cd05e3a01345d73f5144bebabd6f
Jonathan Cox <joncoxhome@gmail.com>
2023-11-21 14:32:07 +0000

    Clean up pythonrc

--- dcc/houdini/python3.9libs/pythonrc.py
+++ dcc/houdini/python3.9libs/pythonrc.py
@@ -1,42 +1,24 @@
 #!/usr/bin/env python
 
+"""Start the Node Manager."""
+
 import logging
-import os
 import sys
 
 import hou
 
 from node_manager import manager
-from node_manager import utils
 
-
+# Setting up logging here, but this can be skipped if it is handled elsewhere.
 logging.basicConfig(
     level=logging.DEBUG,
     format="%(levelname)s:[%(name)s.%(funcName)s:%(lineno)d] %(message)s",
     stream=sys.stdout,
 )
 
-
-if utils.using_rez():
-    manager.initialise_node_manager(
-        background=hou.isUIAvailable(),
-        load_plugin="RezLoad",
-        release_plugin="RezRelease",
-    )
-else:
-    os.environ["NODE_MANAGER_PLUGINS_PATH"] = "/Users/jcox/source/github/node_manager/lib/python/node_manager/plugins"
-
-    # # Disk based load
-    # os.environ["NODE_MANAGER_REPOS"] = "/Users/jcox/hdas"
-    # os.environ["NODE_MANAGER_BASE"] = "/Users/jcox/hdas"
-    # manager.initialise_node_manager(
-    #     background=hou.isUIAvailable(),
-    # )
-
-    # Git based load
-    os.environ["NODE_MANAGER_REPOS"] = "git@github.com:j0nc0x/hda_repo.git"
-    manager.initialise_node_manager(
-        background=hou.isUIAvailable(),
-        load_plugin="GitLoad",
-        release_plugin="GitRelease",
-    )
+# Initialise the Node Manager
+manager.initialise_node_manager(
+    background=hou.isUIAvailable(),
+    load_plugin="RezLoad",
+    release_plugin="RezRelease",
+)
