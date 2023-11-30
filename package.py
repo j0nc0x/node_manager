7b887a50b3635c9004556b9502ef27c6093f61dd
j0nc0x <joncoxhome@gmail.com>
2023-11-30 12:10:53 +0000

    Add note regarding Houdini environment paths.

--- package.py
+++ package.py
@@ -19,5 +19,12 @@ build_command = "{root}/bin/build {install}"
 
 def commands():
     env.PYTHONPATH.append("{root}/lib/python")
+
+    # Note: '&' is used by Houdini to include the default locations for thse paths.
+    # If this is handled elsewhere, either by a "houdini_config" rez package or the
+    # Houdini rez package itself, then the '&' can be removed. See commented out lines
+    # below which can be replaced.
+    #env.HOUDINI_MENU_PATH.append("{root}/dcc/houdini/menu")
+    #env.HOUDINI_PATH.append("{root}/dcc/houdini")
     env.HOUDINI_MENU_PATH.set("{root}/dcc/houdini/menu:&")
     env.HOUDINI_PATH.set("{root}/dcc/houdini:&")
