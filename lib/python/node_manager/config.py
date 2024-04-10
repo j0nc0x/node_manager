74d3cb069631b659c6fb282f3a67af137e415df2
j0nc0x <joncoxhome@gmail.com>
2024-04-10 19:55:47 +0100

    tidy up mistake from merge

--- lib/python/node_manager/config.py
+++ lib/python/node_manager/config.py
@@ -5,8 +5,6 @@
 
 node_manager_config = {
     "background": False,
-    "load_plugin": "GitLoad",
-    "release_plugin": "GitRelease",
     "namespaces": [
         "{user}",
         "{user}.dev",
